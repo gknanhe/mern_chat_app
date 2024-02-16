e7f2c33ec8e2fb25cbe3ee0ae3281e29f60e0bcd
gknanhe <cse18gknanhejd@gmail.com>
2024-02-16 16:03:46 +0530

    added all ui part

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            alt="avatar"
+          />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white bg-blue-500`}>Hi! Whats upp?</div>
+      <div
+        className={`chat-footer opacity-50 text-xs flex gap-1 items-center `}
+      >
+        12:33
+      </div>
+    </div>
+  );
+};
+
+export default Message;
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Conversations from "./Conversations";
+import LogoutButton from "./LogoutButton";
 import Searchbar from "./Searchbar";
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className="border-r border-slate-500 p-4 flex flex-col ">
       <Searchbar />
       <div className="divider px-3"></div>
       <Conversations />
+      <LogoutButton />
     </div>
   );
 };
