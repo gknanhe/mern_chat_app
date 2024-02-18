6c5cfe2fc39c4ab99d311fc8f7e8ffd091dd9d18
gknanhe <cse18gknanhejd@gmail.com>
2024-02-18 17:16:25 +0530

    deploymet

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -15,6 +16,7 @@ dotenv.config();
 // console.log(process.env.PORT);
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 // console.log(PORT);
 app.use(
   cors({
@@ -31,13 +33,16 @@ app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 
 // connectMongoDB();
 
-// app.get("/", (req, res) => {
-//   res.send("hellow CHarLie!!!");
-// });
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+//to serve static files
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectMongoDB();
